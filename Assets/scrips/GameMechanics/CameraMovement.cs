e7c1baf2f065c79f30c4cd07d2e05872acb04e48
Mischa Prudnikov <mishaprudnikov11@gmail.com>
2022-07-16 18:43:19 +0200

    Start menu

---
+++ Assets/scrips/GameMechanics/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace scrips.GameMechanics
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        public InputAction cameraMovementAction;
+
+        private bool _isMoving = false;
+
+        private void OnEnable()
+        {
+            cameraMovementAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            cameraMovementAction.Disable();
+        }
+
+        private void Start()
+        {
+            cameraMovementAction.performed += context => StartCoroutine(RotateCamera(context.ReadValue<float>()));
+        }
+
+        private IEnumerator RotateCamera(float value)
+        {
+            if (!_isMoving)
+            {
+                _isMoving = true;
+                const int totalDeg = 90;
+                const int rotateAmount = 1;
+                var currentDeg = 0;
+                while (totalDeg > currentDeg)
+                {
+                    transform.Rotate(Vector3.up, rotateAmount *value, Space.World);
+                    currentDeg += rotateAmount;
+                    yield return new WaitForSeconds(Time.deltaTime);
+                }
+                _isMoving = false;
+            }
+        }
+    }
+}
---
+++ Assets/scrips/UI/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace scrips.UI
+{
+    public class ButtonManager : MonoBehaviour
+    {
+        public Animator anim;
+
+        public void OnPressedPlay()
+        {
+            anim.Play("levelSelectSwitch");
+        }
+
+        public void OnPressedBack()
+        {
+            anim.Play("levelSelectSwitchBack");
+        }
+
+        public void OnPressedQuit()
+        {
+            Application.Quit();
+        }
+
+        public void LevelLoad(int level)
+        {
+            SceneManager.LoadScene("Level_" + level);
+        }
+    }
+}
